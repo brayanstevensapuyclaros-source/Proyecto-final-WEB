fbdf3dca0b96e617b5221915579ed37d0d6e39f5
Brayan Steven <juanpablocardenascarvajal@gmail.com>
2026-05-28 19:05:28 -0500

    Project WEB.0.1.

--- src/Supabase/config.js
+++ src/Supabase/config.js
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://kjovpyqvtqbueucbrfdc.supabase.co'
+const supabaseUrl = 'https://cbnsfhcexqqdwaosvcmm.supabase.co'
 
 // IMPORTANTE: Ve a tu proyecto en supabase.com
 // Settings > API > Project API keys > anon (public)
 // Pega el valor completo que empieza con "eyJ..."
-const supabaseAnonKey = 'PEGA_AQUI_TU_ANON_KEY'
+const supabaseAnonKey = 'sb_publishable_4fY3DqiiXEt6rKJEn4tZdQ_d3AN1AP2'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
